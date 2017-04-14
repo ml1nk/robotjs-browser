4e5f3a33ae1e53e4d4a7bd75f2213ae728d34980
Marcel Link <marcel.link@t-online.de>
2017-04-14 22:45:52 +0200

    ^ Update dependencies
    # fix path problem with newer versions of socket.io-client

--- robotjs-browser.js
+++ robotjs-browser.js
@@ -26,7 +26,7 @@ var io = require('socket.io')(server, {
 var serve = {
     files: {
         "socket.io" : {
-          "path" : require.resolve("socket.io-client/socket.io.js")
+          "path" : require.resolve("socket.io-client/dist/socket.io.js")
         },
         "setImmediate" : {
           "path" : __dirname + "/client/lib/setImmediate.js"
